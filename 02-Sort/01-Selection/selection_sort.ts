184185616744fdb970e69c66092dd80cd910d83c
Joanna <joanna.lisiak13@gmail.com>
2023-11-03 20:41:23 +0100

    write sort algorithm: selection

---
+++ 02-Sort/01-Selection/selection_sort.ts
@@ -0,0 +1,55 @@
+/**
+ * repeat until no unsorted elements remain:
+	  search unsorted part to find smallest element
+	  swap smallest found element with first element of unsorted part
+ */
+
+const selectionSort = (list: number[]): number[] => {
+  for (let i = 0; i < list.length - 1; i++) {
+    let smallestIndex = i
+
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[j] < list[smallestIndex]) {
+        smallestIndex = j
+      }
+    }
+
+    if (smallestIndex !== i) {
+      const temp = list[i]
+      list[i] = list[smallestIndex]
+      list[smallestIndex] = temp
+    }
+  }
+
+  return list
+}
+
+/**
+ * const selectionSortTwo = (list: number[]): number[] => {
+  const sorted: number[] = []
+
+  while (list.length > 0) {
+    const smallestIndex = findSmallestIndex(list)
+    sorted.push(list.splice(smallestIndex, 1)[0])
+  }
+
+  return sorted
+}
+
+const findSmallestIndex = (list: number[]): number => {
+  let smallestElement = list[0]
+  let smallestIndex = 0
+
+  for (let i = 0; i < list.length - 1; i++) {
+    if (list[i] < smallestElement) {
+      smallestElement = list[i]
+      smallestIndex = i
+    }
+  }
+
+  return smallestIndex
+}
+ */
+
+const list = [4, 6, 3, 8, 1]
+console.log(selectionSort(list)) // [1, 3, 4, 6, 8]
